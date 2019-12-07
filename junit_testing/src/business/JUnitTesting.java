3cb1f2119a9f3ba893d4d3c2205e547d037af9d2
Kkreanne <54691061+Kkreanne@users.noreply.github.com>
2019-12-07 08:14:39 -0500

    JUnit Testing

---
+++ junit_testing/src/business/JUnitTesting.java
@@ -0,0 +1,14 @@
+package business;
+
+public class JUnitTesting {
+
+	private int getSquare() {
+		int nbr = square * square;
+		return nbr;
+	}
+	
+	private String countA() {
+		return str;
+	}
+	
+} 
---
+++ junit_testing/src/ui/JUnitTestingApp.java
@@ -0,0 +1,22 @@
+package ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Console;
+
+public class JUnitTestingApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome\n");		
+		
+		Console.getInt("Please enter a number: ");
+		
+		
+		
+		
+		
+		System.out.println("Goodbye!");
+	}
+
+}
